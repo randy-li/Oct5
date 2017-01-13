c9eaac88ad46d42a22b9077490bd440c0e36d452
Yunpeng Li <yunpengli@Yunpengs-MacBook-Pro.local>
2017-01-12 16:41:44 -0800

    Add jUnit part

---
+++ jUnit/AddOperationTestWithoutAnnotation.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+public class AddOperationTestWithoutAnnotation extends TestCase {
+	
+	public void setUp() throws Exception {
+	}
+
+
+	public void tearDown() throws Exception {
+	}
+
+
+	public void testAdd(){
+		System.out.println("add");
+		int x = 0;
+		int y = 0;
+		AddOperation instance = new AddOperation();
+		int expResult = 0;
+		int result = instance.add(x, y);
+		assertEquals(expResult, result);
+	}
+}
